9e5394393d7eb153b830df8048a976375e947624
Matt Gleich <git@mattglei.ch>
2021-12-19 20:04:35 -0500

    feat: meeting notes generator script
    Signed-off-by: Matt Gleich <git@mattglei.ch>

---
+++ meetings/new.py
@@ -0,0 +1,23 @@
+# This file is meant to create a new meeting note file automatically.
+
+from datetime import datetime
+import os
+
+now = datetime.now()
+ordinal = (
+    "th" if 11 <= now.day <= 13 else {1: "st", 2: "nd", 3: "rd"}.get(now.day % 10, "th")
+)
+
+folder = now.strftime("%Y - %B").lower()
+file = os.path.join(folder, f"{str(now.day)}{ordinal}.md")
+template = now.strftime(
+    f"# %-m{ordinal} of %B\n\n## Hardware\n\n-\n\n## Software\n\n-\n\n## Communications\n\n-\n\n## Other\n\n-\n"
+)
+
+
+if not os.path.isdir(folder):
+    os.makedirs(folder)
+
+with open(file, "w") as note_file:
+    note_file.write(template)
+print(f'Created "{file}" open it with your favorite editor')
